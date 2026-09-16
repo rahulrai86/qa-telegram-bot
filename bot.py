f08a6d944f6e7431a89a043150b5bafec04aabad
rahulrai86 <96014000+rahulrai86@users.noreply.github.com>
2026-09-16 18:50:30 +0530

    Created bot.py

---
+++ bot.py
@@ -0,0 +1,30 @@
+import os
+import requests
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+message = """🤖 QA Telegram Bot
+
+Hello! 👋
+
+This is the first automated message from my cloud-hosted QA bot.
+
+🧪 Manual Testing
+🔌 API Testing
+🤖 Test Automation
+
+More QA content will be coming soon! 🚀
+"""
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print(response.json())
